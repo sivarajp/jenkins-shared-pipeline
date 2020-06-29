653dafae0e7992f0536595b5255491d67ecec599
sivarajp <sivathevan@gmail.com>
2020-06-29 15:50:16 -0400

    Update pythonbuild.groovy

--- vars/pythonbuild.groovy
+++ vars/pythonbuild.groovy
@@ -19,7 +19,22 @@ def call(Map config) {
                     config.branchName = env.BRANCH_NAME
                 }
                 stage ('Test') {
-                    
+
+                }
+
+                if (config.doDockerBuild == 'true') {
+                    stage ('Docker build and push')   {
+                        container ('kaniko') {
+                            sh "executor -f `pwd`/Dockerfile -c `pwd` --insecure --skip-tls-verify --cache=true --destination=sivarajp/${config.repoName}:${config.commitId}" 
+                            config.dockerimage = "sivarajp/${config.repoName}:${config.commitId}"
+                        }
+                    }
+                }
+
+                if (config.doTBSBuild == 'true') {
+                    stage ('TBS Docker build and push')   {
+                        tbsbuild(config)
+                    }
                 }
             } finally {
                // cleanWs()
