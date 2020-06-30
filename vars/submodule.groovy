f3d44ab63e9f4a4c9ce79a30548e57e4f27e032d
sivarajp <sivathevan@gmail.com>
2020-06-30 14:40:58 -0400

    Update submodule.groovy

--- vars/submodule.groovy
+++ vars/submodule.groovy
@@ -5,13 +5,13 @@ def call(Map config) {
             echo $GIT_TOKEN
             git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
             git fetch --all --recurse-submodules
+            git config --local credential.helper "!f() { echo username=\\$GIT_USER; echo password=\\$GIT_TOKEN; }; f"
             curl https://gist.githubusercontent.com/patdunlavey/dcc36b2085dddc22404f805978c0f11d/raw/903a4cbc23a798244f38ce1fb1414125647ff7ba/git-submodule-flatten.sh -o git-submodule-flatten.sh
             chmod +x git-submodule-flatten.sh 
             git add git-submodule-flatten.sh
             git commit -m "Jenkins sumodule commit"
             ./git-submodule-flatten.sh temp-test 
             git commit -m "Jenkins sumodule commit"
-            git config --local credential.helper "!f() { echo username=\\$GIT_USER; echo password=\\$GIT_TOKEN; }; f"
             git push --set-upstream origin temp-test
         ''')
     } 
