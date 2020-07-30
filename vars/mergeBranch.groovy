0ed4170b17401acb29e4d33796fe22b1bc08f6b6
sivarajp <sivathevan@gmail.com>
2020-07-30 09:45:02 -0400

    Update mergeBranch.groovy

--- vars/mergeBranch.groovy
+++ vars/mergeBranch.groovy
@@ -4,6 +4,7 @@ def call(Map config) {
             printenv
             echo $GIT_USER 
             echo $GIT_TOKEN
+            git config --local credential.helper "!f() { echo username=\\$GIT_USER; echo password=\\$GIT_TOKEN; }; f"
             git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
             git fetch --all
             git checkout -b temp #makes a new branch from current detached HEAD
@@ -11,7 +12,6 @@ def call(Map config) {
             git merge temp
             git branch -d temp
             #git fetch --no-tags --force --progress -- $GIT_URL +refs/heads/*:refs/remotes/origin/*
-            git config --local credential.helper "!f() { echo username=\\$GIT_USER; echo password=\\$GIT_TOKEN; }; f"
             git push
         ''')
     } 
