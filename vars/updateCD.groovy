a5aa9e066a1ca3b52170f128a62a146bac68ead5
sivarajp <sivathevan@gmail.com>
2021-01-11 03:24:29 -0500

    Update updateCD.groovy

--- vars/updateCD.groovy
+++ vars/updateCD.groovy
@@ -7,8 +7,7 @@ def call(Map config) {
     // )    
     sh "mkdir $HOME/tanzu-bank-cd"
     dir("$HOME/tanzu-bank-cd") {
-        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-credentials', url: 'https://github.com/sivarajp/tanzu-bank-cd']]])
-        sh """
+checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', honorRefspec: true, noTags: false, reference: '', shallow: false]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-credentials', url: 'https://github.com/sivarajp/tanzu-bank-cd']]])        sh """
         touch siva.txt
         git add .
         git commit -m "siva"
@@ -33,7 +32,7 @@ def call(Map config) {
     // } 
 }
 
-// git config --local credential.helper "!f() { echo username=\\$GIT_USER; echo password=\\$GIT_TOKEN; }; f"
+//             git config --local credential.helper "!f() { echo username=\\$GIT_USER; echo password=\\$GIT_TOKEN; }; f"
 //             git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 //             git fetch --all
 //             git checkout -b temp #makes a new branch from current detached HEAD
