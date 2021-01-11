7a31d7f8a163e7af8bbaaee1a5016797ad2d99a9
sivarajp <sivathevan@gmail.com>
2021-01-11 01:46:21 -0500

    Update tbsbuild.groovy

--- vars/tbsbuild.groovy
+++ vars/tbsbuild.groovy
@@ -27,11 +27,12 @@ def call(Map config) {
                                 kp image trigger ${config.repoName} -n acme-builds
                                 sleep 30
                                 X=$( kp image status  user-service -n acme-builds | grep Status  | cut -d':' -f2 | xargs )
-                                while [ "$X" != "Ready" ]
-                                do
-                                    sleep 10
-                                    X=$( kp image status  user-service -n acme-builds | grep Status  | cut -d':' -f2 | xargs )
-                                done
+                                echo $X
+                                // while [ "$X" != "Ready" ]
+                                // do
+                                //     sleep 10
+                                //     X=$( kp image status  user-service -n acme-builds | grep Status  | cut -d':' -f2 | xargs )
+                                // done
                             """
                             DOCKER_IMAGE = sh(script: "export KUBECONFIG=/var/kp/kube/config  && kp image status  ${config.repoName} -n acme-builds | grep LatestImage  | cut -d':' -f2 | xargs ", returnStdout: true).trim()
                             
