d3f0746d07eb5295ac965754d358d13e7c6d05b9
sivarajp <sivathevan@gmail.com>
2021-01-11 04:09:11 -0500

    Update updateCD.groovy

--- vars/updateCD.groovy
+++ vars/updateCD.groovy
@@ -6,22 +6,23 @@ def call(Map configmap) {
     //    branch: "master"
     // )    
 
-    dir("$HOME/tanzu-bank-cd") {
-        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'LocalBranch', localBranch: 'master']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-credentials', url: 'https://github.com/sivarajp/tanzu-bank-cd']]])        
+    
         script {
-        echo ${configmap.reponame}
-        cd ${configmap.reponame}
-        withCredentials([usernamePassword(credentialsId: 'github-credentials', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_TOKEN',)]) {
-            sh """
-            git config --local credential.helper "!f() { echo username=\\$GIT_USER; echo password=\\$GIT_TOKEN; }; f"
-            git config --global user.name $GIT_USER
-            git config --global user.password $GIT_TOKEN
-            git add .
-            git commit -m "pipeline commit"
-            git push --set-upstream origin master
-            git push
-            """
-            }
+            dir("$HOME/tanzu-bank-cd") {
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'LocalBranch', localBranch: 'master']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-credentials', url: 'https://github.com/sivarajp/tanzu-bank-cd']]])        
+                echo ${configmap.reponame}
+                cd ${configmap.reponame}
+                withCredentials([usernamePassword(credentialsId: 'github-credentials', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_TOKEN',)]) {
+                    sh """
+                    git config --local credential.helper "!f() { echo username=\\$GIT_USER; echo password=\\$GIT_TOKEN; }; f"
+                    git config --global user.name $GIT_USER
+                    git config --global user.password $GIT_TOKEN
+                    git add .
+                    git commit -m "pipeline commit"
+                    git push --set-upstream origin master
+                    git push
+                    """
+                }
         }
     }
     // withCredentials([usernamePassword(credentialsId: 'github-credentials', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_TOKEN',)]) {
