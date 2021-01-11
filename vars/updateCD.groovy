278cdb56571f570541380c9d03486c613e8e3872
sivarajp <sivathevan@gmail.com>
2021-01-11 12:47:49 -0500

    Update updateCD.groovy

--- vars/updateCD.groovy
+++ vars/updateCD.groovy
@@ -14,7 +14,7 @@ def call(Map configmap) {
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'LocalBranch', localBranch: 'master']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-credentials', url: 'https://github.com/sivarajp/tanzu-bank-cd']]])        
                 sh """
                     ls -lrt 
-                    echo ${configmap.dockerimage} | sed s/\\//\\\\\\//g
+                    echo ${configmap.dockerimage} | sed "s/\\//\\\\\\//g"
                     sed -i "/^\\([[:space:]]*image: \\).*/s//\\1${configmap.dockerimage}/"  ./${configmap.repoName}/${configmap.repoName}.yml
                 """
                 withCredentials([usernamePassword(credentialsId: 'github-credentials', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_TOKEN',)]) {
