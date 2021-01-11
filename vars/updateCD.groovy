1381bcac31ef5adb3adff34ce3d29d55de8b60ad
sivarajp <sivathevan@gmail.com>
2021-01-11 11:39:13 -0500

    Update updateCD.groovy

--- vars/updateCD.groovy
+++ vars/updateCD.groovy
@@ -8,12 +8,12 @@ def call(Map configmap) {
 
 
 
-    
+        configmap.dockerimage = "harbor.kubeeight.com\\/ecommerce\\/user-service@sha256:df0d8e46d88be25958c33d4291d7f0ba44a6fb58de02d479a41701f399908107"
         script {
             dir("$HOME/tanzu-bank-cd") {
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'LocalBranch', localBranch: 'master']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-credentials', url: 'https://github.com/sivarajp/tanzu-bank-cd']]])        
                 sh """
-                    ls -lrt
+                    ls -lrt 
                     sed -i "/^\\([[:space:]]*image: \\).*/s//\\1${configmap.dockerimage}/"  ./${configmap.repoName}/${configmap.repoName}.yml
                 """
                 withCredentials([usernamePassword(credentialsId: 'github-credentials', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_TOKEN',)]) {
